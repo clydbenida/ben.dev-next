eb33efe9c51f3cc22ae197c4ecc3f23bd3b58d0f
bbenida <clyd.benidz@gmail.com>
2024-10-12 12:24:52 +0800

    CHANGE: Hid project page contents

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,5 @@
 import ProjectCard from "@/components/ProjectCard";
+import UnderDev from "@/components/UnderDev";
 import { contentfulClient } from "@/contentful";
 
 export default async function ProjectsPage() {
@@ -6,18 +7,22 @@ export default async function ProjectsPage() {
     content_type: "project",
   });
 
-  return (
-    <div className="mt-40">
-      <h4 className="text-sm italic font-light">Projects</h4>
-      <h1 className="text-3xl font-bold mb-20">
-        Projects
-      </h1>
+  // return (
+  //   <div className="mt-40">
+  //     <h4 className="text-sm italic font-light">Projects</h4>
+  //     <h1 className="text-3xl font-bold mb-20">
+  //       Projects
+  //     </h1>
 
-      <div>
-        {projectEntries.items?.map((item: Record<string, any>, key: number) => (
-          <ProjectCard projectFields={item.fields} key={key} />
-        ))}
-      </div>
-    </div>
-  );
+  //     <div>
+  //       {projectEntries.items?.map((item: Record<string, any>, key: number) => (
+  //         <ProjectCard projectFields={item.fields} key={key} />
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
+
+  return (
+    <UnderDev />
+  )
 }
