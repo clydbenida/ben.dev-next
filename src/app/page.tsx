1b8e27710aab28278f306da3f8d28ba22c7d95d5
bbenida <clyd.benidz@gmail.com>
2024-10-12 14:52:38 +0800

    FEAT: Revised layout for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ProjectHighlight from "@/components/ProjectHighlight";
+import { IoIosSend } from "react-icons/io";
 import Hero from "@/components/Hero/Hero";
 import Section, { SectionContent, SectionTitle } from "@/components/Section";
 import Image from "next/image";
@@ -7,18 +8,14 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <div className="mt-5">
-      <div className="hero bg-base-200 pt-24">
+      <div className="hero bg-base-200 pt-24 mb-2">
         <div className="hero-content flex-col p-0 w-full">
           <Hero />
           <div className="text-justify w-full">
             <h1 className="font-medium py-4 text-center backdrop-blur-md bg-gray-200 rounded-lg mb-5">
-              Hi! I'm a web developer based in the Philippines.
+              Hi! This is a random quote.
             </h1>
-            <div className="flex sm:flex-row flex-col-reverse justify-between items-center">
-              <div className="text-center sm:text-left">
-                <h1 className="text-2xl sm:text-4xl font-bold">Benedict Clyd Benida</h1>
-                <p className="text-sm sm:mt-3 sm:text-md">Fullstack Developer (Node.js, React.js)</p>
-              </div>
+            <div className="grid grid-cols-5 items-center">
               <Image 
                 src='/headshot-1.jpeg' 
                 alt="Test"
@@ -26,6 +23,13 @@ export default async function Home() {
                 height={125} 
                 className="object-none object-center h-[100px] w-[100px] rounded-full border-2 mb-5 border-gray-400"  
               />
+              <div className="text-center sm:ml-2 sm:text-left col-span-3">
+                <h1 className="text-xl sm:text-2xl font-bold">Benedict Clyd Benida</h1>
+                <p className="text-sm  sm:text-md">Fullstack Developer (Node.js, React.js)</p>
+              </div>
+              <Link href='/contact' className="justify-self-end bg-gray-200 py-2 px-4 rounded-lg">
+                <IoIosSend size={25} />
+              </Link>
             </div>
             {/* <p className="py-6">
               I am a motivated web developer based in the Philippines 🇵🇭.
