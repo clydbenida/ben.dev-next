4e0d2bcb60e0b0d3d9a1b96d7e16c033d4ca3ad5
bbenida <clyd.benidz@gmail.com>
2024-10-14 11:41:56 +0800

    - Transitioned from grid to flex

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -22,36 +22,45 @@ export default function HeroSection() {
     setRandomCallout(randomStrings[rNum])
   }, [])
   return (
-      <div className="hero bg-base-200 pt-24 mb-2">
-        <div className="hero-content flex-col p-0 w-full">
-          <Hero />
-          <div className="text-justify w-full">
-            <h1 className="font-medium py-4 px-2 text-center backdrop-blur-md bg-gray-200 rounded-lg mb-5">
-              {randomCallout}
-            </h1>
-            <div className="grid grid-cols-5 items-center">
-              <Image 
-                src='/headshot-1.jpeg' 
-                alt="Test"
-                width={125} 
-                height={125} 
-                className="object-none object-center h-[100px] w-[100px] rounded-full border-2 mb-5 border-gray-400"  
-              />
-              <div className="text-center sm:ml-2 sm:text-left col-span-3">
-                <h1 className="text-xl sm:text-2xl font-bold">Benedict Clyd Benida</h1>
-                <p className="text-sm  sm:text-md">Fullstack Developer (Node.js, React.js)</p>
-              </div>
-              <Link href='/contact' className="justify-self-end bg-gray-200 py-2 px-4 rounded-lg">
+    <div className="hero bg-base-200 pt-24 mb-2">
+    <div className="hero-content flex-col p-0 w-full">
+      <Hero />
+      <div className="text-justify w-full">
+        <h1 className="font-medium py-4 text-center backdrop-blur-md bg-gray-200 rounded-lg mb-5">
+          {randomCallout}
+        </h1>
+        <div className="flex items-center flex-col md:flex-row mb-5">
+          <Image 
+            src='/headshot-1.jpeg' 
+            alt="Test"
+            width={125} 
+            height={125} 
+            className="object-none object-center h-[100px] w-[100px] rounded-full border-2 mb-4 md:mb-0 border-gray-400"  
+          />
+          <div className="text-left sm:ml-4 sm:text-left grow flex flex-row w-full md:w-auto items-center">
+            <div className="grow">
+              <h1 className="text-xl sm:text-2xl font-bold">Benedict Clyd Benida</h1>
+              <p className="text-sm sm:text-md mb-0">Fullstack Developer (Node.js, React.js)</p>
+            </div>
+            <div className="">
+              <Link href='/contact' className="block bg-gray-200 py-2 px-4 rounded-lg">
                 <IoIosSend size={25} />
               </Link>
             </div>
-            {/* <p className="py-6">
-              I am a motivated web developer based in the Philippines 🇵🇭.
-              Currently I am working as a Jr. Fullstack developer using MERN
-              Stack <strong>(MySql, Express.js, React.js, and Node.js)</strong>.
-            </p> */}
+          </div>
+          <div className="hidden">
+            <Link href='/contact' className="block bg-gray-200 py-2 px-4 rounded-lg">
+              <IoIosSend size={25} />
+            </Link>
           </div>
         </div>
+        {/* <p className="py-6">
+          I am a motivated web developer based in the Philippines 🇵🇭.
+          Currently I am working as a Jr. Fullstack developer using MERN
+          Stack <strong>(MySql, Express.js, React.js, and Node.js)</strong>.
+        </p> */}
       </div>
-  )
+    </div>
+  </div>
+)
 }
