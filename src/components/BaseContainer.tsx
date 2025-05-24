45d9970325fab5f2d2de1d0677573bbf5f69cc34
bbenida <clyd.benida@gmail.com>
2025-05-24 14:45:38 +0800

    Reworked herosection

--- src/components/BaseContainer.tsx
+++ src/components/BaseContainer.tsx
@@ -1,7 +1,7 @@
 
 export default function BaseContainer(props: React.PropsWithChildren & React.HTMLProps<HTMLDivElement>) {
   return (
-    <div className={`md:w-[70vw] lg:w-[733px] ${props.className}`}>
+    <div className={`md:w-[70vw] lg:w-[733px] px-4 ${props.className}`}>
       {props.children}
     </div>
   )
